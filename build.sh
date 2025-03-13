c761b659a374f130a9321033ce447e49806041ce
Julia Evans <julia@jvns.ca>
2025-03-13 15:40:35 -0400

    remove build file

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ mkdir -p public
 esbuild main.js --bundle --loader:.html=text --outfile=bundle.js --alias:vue=vue/dist/vue.esm-bundler.js
 cp index.html public
 cp -R static public
-cp bundle.js public
+mv bundle.js public
